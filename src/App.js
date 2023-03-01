87382acd42425afd5c10b674afe53f2f0327accb
Jakub Gluch <jakub.gluch92@gmail.com>
2023-03-01 18:48:42 +0100

    created globalStyle, Theme, installed normalize,

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyle } from "./features/Theme/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { Normalize } from "styled-normalize";
+import ExciseTaxCalculator from "./features/ExciseTaxCalculator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <GlobalStyle />
+      <Normalize />
+      <ExciseTaxCalculator />
+    </ThemeProvider>
   );
 }
 
---
+++ src/features/Theme/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+    box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Inter', sans-serif;
+        word-break: break-word;
+        transition: background 0.3s;
+        padding: 115px 40px;
+        background-color: ${({ theme }) => theme.colorNames.white};
+        letter-spacing: 0.05em;
+    }
+`;
