c7923489f7d187ce768fb29a8620c92acea83162
Jakub Gluch <jakub.gluch92@gmail.com>
2023-03-08 23:35:48 +0100

    added hooks for AgGrid tbc...

--- src/features/ExciseTaxCalculator/CSVImport/Table/index.js
+++ src/features/ExciseTaxCalculator/CSVImport/Table/index.js
@@ -1,19 +1,29 @@
+import { useState, useMemo } from "react";
 import { AgGridReact } from "ag-grid-react"; // the AG Grid React Component
 import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
 import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
 
 const AgGrid = ({ rowData }) => {
-  const columnDefs = [
-    { headerName: "Lp.", field: "0", filter: true, editable: true },
-    { headerName: "Nazwa", field: "1", filter: true, editable: true },
-    { headerName: "Ilość", field: "2", filter: true, editable: true },
-  ];
+  const [columnDefs] = useState([
+    { headerName: "Lp.", field: "0" , width: 100},
+    { headerName: "Nazwa", field: "1" , width: 400},
+    { headerName: "Ilość", field: "2" , width: 100},
+  ]);
+
+  const defaultColDef = useMemo(() => {
+    return {
+      resizable: true,
+      filter: true,
+      editable: true,
+    };
+  }, []);
 
   return (
     <div className="ag-theme-alpine" style={{ width: 1000, height: 1000 }}>
       <AgGridReact
         rowData={rowData}
         columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
         animateRows={true}
         rowSelection="multiple"
       />
